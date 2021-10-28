7fa4763a1b2199fdfb3a09e6b757d29424f38af6
rasjs <93289301+rasjs@users.noreply.github.com>
2021-10-28 18:51:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //window.alert("Seja bem-vindo!")
+var h2HTML5 = document.getElementById("tagH2_HTML5")
+
+h2HTML5.addEventListener("mouseover", mudarCorDeFundoHTML5, false)
+
+function mudarCorDeFundoHTML5 () {
+	document.body.style.background="#48CDB8"
+	//window.alert("Olá!")
+	}
