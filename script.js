82bd465df69999280a8579e291822194eb559190
rasjs <93289301+rasjs@users.noreply.github.com>
2021-10-28 21:56:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-//window.alert("Seja bem-vindo!")
 var h2HTML5 = document.getElementById("tagH2_HTML5")
 
 h2HTML5.addEventListener("mouseover", mudarCorDeFundoHTML5, false)
 
 function mudarCorDeFundoHTML5 () {
 	document.body.style.background="#4CC3DB"
-	//window.alert("Olá!")
-	}
+}
